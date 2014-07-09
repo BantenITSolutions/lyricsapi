9379f2ea2964719bd7cb91b0019defac97524c86
Daniel <daniel@natur-kultur.eu>
2014-07-09 12:18:34 +0200

    idk

--- lyrics.php
+++ lyrics.php
@@ -1,5 +1,6 @@
 <?php
-if($argc == 3)
+error_reporting(E_ALL);
+if(isset($argc) && $argc == 3)
 {
 $nl = "\n";
 $artist = $argv[1];
@@ -11,22 +12,11 @@ else
   $artist = $_REQUEST['artist'];
   $song = $_REQUEST['song'];
 }
-$song = str_replace(" ","",strtolower($song));
-$artist = strtolower($artist);
-function ge($url)
-{
-  $ch = curl_init( $url );
-  curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-  curl_setopt( $ch, CURLOPT_HEADER, true );
-  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-  curl_setopt( $ch, CURLOPT_USERAGENT, "PHP55");
-  $response = preg_split( '/([\r\n][\r\n])\\1/', curl_exec( $ch ));
-  $response = preg_split( '/([\r\n][\r\n]){2}/', curl_exec( $ch ),2);
-  curl_close( $ch );
-  return $response[1];
-}
+$song = fix(strtolower($song));
+$artist = fix(strtolower($artist));
 $dom = new DOMDocument();
-@$dom->loadHTML(ge("http://www.azlyrics.com/lyrics/{$artist}/{$song}.html"));
+$html = ge("http://www.azlyrics.com/lyrics/{$artist}/{$song}.html");
+@$dom->loadHTML($html);
 $b = $dom->getElementsByTagName('b');
 $header = $b->item(0);
 $div = $header->nextSibling->nextSibling->nextSibling;
@@ -36,6 +26,23 @@ foreach ($children as $child)
 {
   $innerHTML .= $div->ownerDocument->saveHTML($child);
 }
-$result = str_replace("<br>",$nl,substr($innerHTML,10));
+$result = str_replace("<br>",$nl,substr($innerHTML,9));
 echo "Lyrics for: " . $header -> textContent . $nl;
 echo $result;
+function fix($a)
+{
+  return str_replace(" ","",str_replace(".","",$a));
+}
+function ge($url)
+{
+  $ch = curl_init( $url );
+  curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+  curl_setopt( $ch, CURLOPT_HEADER, true );
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+  curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:32.0) Gecko/20100101 Firefox/32.0");
+  $a = curl_exec( $ch );
+  $response = preg_split( '/([\r\n][\r\n])\\1/', $a);
+  $response = preg_split( '/([\r\n][\r\n]){2}/', $a,2);
+  curl_close( $ch );
+  return $response[1];
+}
